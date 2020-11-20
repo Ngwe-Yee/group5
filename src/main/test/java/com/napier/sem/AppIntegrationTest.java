04312ac00edb299c6ff5da118ca723aaddb0f83f
40478654 <40478654@live.napier.ac.uk>
2020-11-20 10:21:14 +0630

    Error fixing... [40478654]

--- src/main/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/main/test/java/com/napier/sem/AppIntegrationTest.java
@@ -19,9 +19,32 @@ public class AppIntegrationTest
         app.connect("localhost:33060");
     }
 
+    // Unit Testing for if the city is null.
     @Test
-    void testCity()
+    void printCityTestNull()
     {
-        ArrayList<City> emp = app.getAllCity9();
+        app.printCities9(null);
+    }
+
+    // Unit Testing for if the city is empty.
+    @Test
+    void printCityEmpty()
+    {
+        ArrayList<City> cities = new ArrayList<City>();
+        app.printCities9(cities);
+    }
+
+    // Data are manually input for unit testing
+    @Test
+    void printCity()
+    {
+        ArrayList<City> cities = new ArrayList<City>();
+        City emp = new City();
+        emp.Name = "Rangoon";
+        emp.CountryCode = "MMR";
+        emp.District = "Rangoon";
+        emp.Population = 3361700;
+        cities.add(emp);
+        app.printCities9(cities);
     }
 }
