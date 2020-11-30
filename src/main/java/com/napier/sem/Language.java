453e2f284ef8896165f6866577ad1d0374d2771d
40478654 <40478654@live.napier.ac.uk>
2020-11-30 12:02:53 +0630

    langauage class is added .[40478654]

---
+++ src/main/java/com/napier/sem/Language.java
@@ -0,0 +1,24 @@
+package com.napier.sem;
+
+public class Language {
+    /**
+     * City ID
+     */
+    public String CountryCode;
+
+    /**
+     * City name
+     */
+    public String Language;
+
+    /**
+     * City CountryCode
+     */
+    public String IsOfficial;
+
+    /**
+     * City District
+     */
+    public long Percentage;
+
+}
